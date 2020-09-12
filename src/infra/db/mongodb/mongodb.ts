2fea832daf33ba09715d9fa0a43a3d8d943f28fb
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-11 21:55:14 -0300

    ✨ add a class to manage database connection

---
+++ src/infra/db/mongodb/mongodb.ts
@@ -0,0 +1,15 @@
+import mongoose, { Connection } from 'mongoose';
+import env from '../../../main/config/env';
+
+export class MongoDB {
+    private static db: Connection = mongoose.connection;
+    
+    public static async connect(): Promise<void> {
+        try {
+            mongoose.connect(env.mongoUrl, { useNewUrlParser: true });
+            this.db.once('open', () => console.log.bind(console, `MongoDB is running at ${env.port}`))
+        } catch (error) {
+            this.db.on('error', console.error.bind(console, `MongoDB connection error: ${error}`));
+        }
+    }
+}
