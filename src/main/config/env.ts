4fcd0da8396779800fcc591ed94fa125a1bc090a
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-16 09:30:43 -0300

    🔧 update database configuration string

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
+import UserDB from './../../../password';
+
 export default {
-    mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-express-api',
+    mongoUrl: process.env.MONGO_URL || `mongodb+srv://${UserDB.user}:${UserDB.password}@cluster0.xavng.azure.mongodb.net/dictionary?retryWrites=true&w=majority`,
     port: process.env.PORT || 3033,
     jwtSecret: process.env.JWT_SECRET || 'lala#2jdsf'
 }
