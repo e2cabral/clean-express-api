0c6b906c661b1ad5b8c31cda0ed1d8fe571128b6
Edson Cabral <edson.cabral@trescon.com.br>
2020-09-16 09:32:00 -0300

    🔧 add property to use unified topology

--- src/infra/db/mongodb/mongodb.ts
+++ src/infra/db/mongodb/mongodb.ts
@@ -6,7 +6,7 @@ export class MongoDB {
     
     public static async connect(): Promise<void> {
         try {
-            mongoose.connect(env.mongoUrl, { useNewUrlParser: true });
+            mongoose.connect(env.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
             this.db.once('open', () => console.log.bind(console, `MongoDB is running at ${env.port}`))
         } catch (error) {
             this.db.on('error', console.error.bind(console, `MongoDB connection error: ${error}`));
